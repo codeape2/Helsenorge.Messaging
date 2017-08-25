fdbe387ac8878311a1ce4f0e6e08376bbd8d7508
Bernt Røskar Brenna <bernt.roskar.brenna@ehelse.no>
2017-08-25 09:07:12 +0200

    Extension methods for SendAndContinue/SendAndWait (sans Async)

---
+++ src/Helsenorge.Messaging/SyncExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Helsenorge.Messaging.Abstractions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Helsenorge.Messaging.Abstractions
+{
+    public static class SyncExtensions
+    {
+        public static void SendAndContinue(this IMessagingClient messagingClient, ILogger logger, OutgoingMessage message)
+        {
+            Task.Run(async () => await messagingClient.SendAndContinueAsync(logger, message)).GetAwaiter().GetResult();
+        }
+
+        public static XDocument SendAndWait(this IMessagingClient messagingClient, ILogger logger, OutgoingMessage message)
+        {
+            return Task.Run(async () => await messagingClient.SendAndWaitAsync(logger, message)).GetAwaiter().GetResult();
+        }
+    }
+}
